6adb4ed62b11f3f4a895f2ee98a282d57320165e
AnhMap <nguyenlephong1997@gmail.com>
2019-01-13 12:37:23 +0700

    update api filter lcoation and date dont busy

--- Controllers/QueryController.cs
+++ Controllers/QueryController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TemplateWebApiPhucThinh.Data.Model;
-
+using System.Collections;
 namespace TemplateWebApiPhucThinh.Controllers
 {
     [Route("[controller]")]
@@ -42,23 +42,93 @@ namespace TemplateWebApiPhucThinh.Controllers
         }
 
         [HttpGet]
-        [Route("LocThanhPhoVaNgayXeRanh")]
-        public IActionResult LocThanhPhoVaNgayXeRanh(string location ,string dateStart)
+        [Route("FilterLocationAndDateDontBusy")]
+        public IActionResult LocThanhPhoVaNgayXeRanh(string location ,string dateStart, string dateEnd)
         {
-            DateTime bd = Convert.ToDateTime(dateStart);
+            if(String.IsNullOrWhiteSpace(location)){
+                DateTime dateRental = Convert.ToDateTime(dateStart);
+                DateTime dateReturn = Convert.ToDateTime(dateEnd);
+                Console.WriteLine(dateRental);
+                Console.WriteLine(dateReturn);
+
+                var listCarAutoDontBusy = (
+                    from _car in context.Car 
+                    where !( 
+                        from _orders in context.Orders select _orders.NameCar 
+                    ).Contains(_car.Id) select _car
+                ).Distinct().ToList();
+                // SELECT * from Car c WHERE c.Id NOT IN (SELECT od.nameCar from Orders od )
+
+                var ListCar =
+                    (from _partner in context.Partner
+                    join _partnerCar in context.PartnerCar on _partner.Id equals _partnerCar.IdPartner
+                    join _car in context.Car on _partnerCar.IsCar equals _car.Id
+                    // join _location in context.Location on _partner.Id equals _location.Country
+                    join _order in context.Orders on _car.Id equals _order.NameCar
+                    // where _location.City == location
+                    where _order.CarReturnDay < dateRental || _order.DateOfhire > dateReturn
+                    select _car
+                    ).Distinct().ToList();
             
-            var ListCar =
-         (from _partner in context.Partner
-          join _partnerCar in context.PartnerCar on _partner.Id equals _partnerCar.IsCar
-          join _car in context.Car on _partnerCar.IsCar equals _car.Id
-          join _location in context.Location on _partner.Id equals _location.Country
-          where _location.City == location
-          where _car.PurchDate <bd 
+                // SELECT  od.dateOfHire, od.payDate from Partner p 	
+                //     JOIN Partner_Car pc on pc.IdPartner = p.Id
+                // 	JOIN Car c ON c.id = pc.IsCar
+                // 	JOIN Orders od ON od.nameCar = c.Id
+                // 	WHERE  od.payDate < '2019-01-06' or od.dateOfHire > '2019-01-08'
 
+                List<Car> array = new List<Car>();
 
-          select _car );
+                foreach(var item in ListCar){
+                    array.Add(item);
+                }
 
-            return Ok(ListCar);
+                foreach(var item in listCarAutoDontBusy){
+                    array.Add(item);
+                }
+                return Ok(array);
+            }else{
+                DateTime dateRental = Convert.ToDateTime(dateStart);
+                DateTime dateReturn = Convert.ToDateTime(dateEnd);
+                Console.WriteLine(dateRental);
+                Console.WriteLine(dateReturn);
+
+                var listCarAutoDontBusy = (
+                    from _car in context.Car 
+                    where !( 
+                        from _orders in context.Orders select _orders.NameCar 
+                    ).Contains(_car.Id) select _car
+                ).Distinct().ToList();
+                // SELECT * from Car c WHERE c.Id NOT IN (SELECT od.nameCar from Orders od )
+
+                var ListCar =
+                    (from _partner in context.Partner
+                    join _partnerCar in context.PartnerCar on _partner.Id equals _partnerCar.IdPartner
+                    join _car in context.Car on _partnerCar.IsCar equals _car.Id
+                    join _location in context.Location on _partner.Id equals _location.Id
+                    join _order in context.Orders on _car.Id equals _order.NameCar
+                    where _location.City == location
+                    where _order.CarReturnDay < dateRental || _order.DateOfhire > dateReturn
+                    select _car
+                    ).Distinct().ToList();
+            
+                // SELECT  od.dateOfHire, od.payDate from Partner p 	
+                //     JOIN Partner_Car pc on pc.IdPartner = p.Id
+                // 	JOIN Car c ON c.id = pc.IsCar
+                // 	JOIN Orders od ON od.nameCar = c.Id
+                // 	WHERE  od.payDate < '2019-01-06' or od.dateOfHire > '2019-01-08'
+
+                List<Car> array = new List<Car>();
+
+                foreach(var item in ListCar){
+                    array.Add(item);
+                }
+
+                foreach(var item in listCarAutoDontBusy){
+                    array.Add(item);
+                }
+                return Ok(array );
+            }
+            
         }
     }
 }
