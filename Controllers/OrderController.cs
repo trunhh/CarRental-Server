0b0f28e51580c6dfc1088785f39f9ecf87a3d1d3
AnhMap <nguyenlephong1997@gmail.com>
2019-01-21 14:34:03 +0700

    update version

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -12,7 +12,6 @@ using TemplateWebApiPhucThinh.Repository.IRepository;
 
 namespace TemplateWebApiPhucThinh.Controllers
 {
-    [Authorize()]
     [Route("[controller]")]
     [ApiController]
     public class OrdersController : ControllerBase
@@ -31,18 +30,20 @@ namespace TemplateWebApiPhucThinh.Controllers
         [Route("Create")]
         public IActionResult Create([FromBody] Orders Orders)
         {
-            var claims = User.Claims.Select(claim => new { claim.Type, claim.Value }).ToDictionary( t => t.Type, t => t.Value);
-            if(claims.ContainsKey("name")){
-                if( claims["name"].Equals("ADMIN") || claims["name"].Equals("MANAGER") || claims["name"].Equals("PARTNER") ){
+            // var claims = User.Claims.Select(claim => new { claim.Type, claim.Value }).ToDictionary( t => t.Type, t => t.Value);
+            // if(claims.ContainsKey("name")){
+            //     if( claims["name"].Equals("ADMIN") || claims["name"].Equals("MANAGER") || claims["name"].Equals("PARTNER") ){
                     Orders.Id = Guid.NewGuid() + "";
                     Orders.IsDelete=false;
                     return Ok(_repository.Create(Orders));
-                }
-            }else{
-                return Forbid();
-            }
-                return Forbid();
+            //     }
+            // }else{
+            //     return Forbid();
+            // }
+            //     return Forbid();
         }
+        
+        [Authorize()]
         [HttpGet]
         [Route("GetById/{id}")]
         public IActionResult GetById(string id)
@@ -59,6 +60,7 @@ namespace TemplateWebApiPhucThinh.Controllers
           
         }
 
+        [Authorize()]
         [HttpDelete]
         [Route("Delete/{id}")]
         public IActionResult Delete(string id)
@@ -74,6 +76,8 @@ namespace TemplateWebApiPhucThinh.Controllers
                 return Forbid();
             
         }
+
+        [Authorize()]
         [HttpPut]
         [Route("Update/{id}")]
         public IActionResult Update(string id, [FromBody] Orders Orders)
@@ -89,22 +93,25 @@ namespace TemplateWebApiPhucThinh.Controllers
                 return Forbid();
             
         }
+
+
         [HttpGet]
         [Route("Paging/pagesize/pageNow")]
         public IActionResult Paging(int pagesize, int pageNow)
         {
-             var claims = User.Claims.Select(claim => new { claim.Type, claim.Value }).ToDictionary( t => t.Type, t => t.Value);
-            if(claims.ContainsKey("name")){
-                if( claims["name"].Equals("ADMIN") || claims["name"].Equals("MANAGER") || claims["name"].Equals("PARTNER") ){
+            //  var claims = User.Claims.Select(claim => new { claim.Type, claim.Value }).ToDictionary( t => t.Type, t => t.Value);
+            // if(claims.ContainsKey("name")){
+            //     if( claims["name"].Equals("ADMIN") || claims["name"].Equals("MANAGER") || claims["name"].Equals("PARTNER") ){
                      return Ok(_repository.Paging(pagesize, pageNow, "color"));
-                }
-            }else{
-                return Forbid();
-            }
-                return Forbid();
+            //     }
+            // }else{
+            //     return Forbid();
+            // }
+            //     return Forbid();
         }
 
-         [HttpGet]
+        [Authorize()]
+        [HttpGet]
         [Route("CountOfPaging/pagesize/pageNow")]
         public IActionResult CountOfPaging(int pagesize, int pageNow)
         {
@@ -118,22 +125,26 @@ namespace TemplateWebApiPhucThinh.Controllers
             }
                 return Forbid();
         }
-         [HttpGet]
+
+
+        [HttpGet]
         [Route("CountAll/pagesize/pageNow")]
         public IActionResult CountAll(int pagesize, int pageNow)
         {
-            var claims = User.Claims.Select(claim => new { claim.Type, claim.Value }).ToDictionary( t => t.Type, t => t.Value);
-            if(claims.ContainsKey("name")){
-                if( claims["name"].Equals("ADMIN") || claims["name"].Equals("MANAGER") || claims["name"].Equals("PARTNER") ){
+            // var claims = User.Claims.Select(claim => new { claim.Type, claim.Value }).ToDictionary( t => t.Type, t => t.Value);
+            // if(claims.ContainsKey("name")){
+            //     if( claims["name"].Equals("ADMIN") || claims["name"].Equals("MANAGER") || claims["name"].Equals("PARTNER") ){
                       return Ok(_repository.CountAll());
-                }
-            }else{
-                return Forbid();
-            }
-            return Forbid();
+            //     }
+            // }else{
+            //     return Forbid();
+            // }
+            // return Forbid();
            
 
         }
+
+        [Authorize()]
         [HttpDelete]
         [Route("DeleteEnable/{id}")]
         public IActionResult DeleteEnable(string id)
@@ -154,6 +165,7 @@ namespace TemplateWebApiPhucThinh.Controllers
            
         }
 
+        [Authorize()]
         [HttpGet]
         [Route("PagingCondition/pagesize/pageNow/condition")]
         public IActionResult PagingCondition(int pagesize, int pageNow,string condition)
@@ -168,7 +180,7 @@ namespace TemplateWebApiPhucThinh.Controllers
             }
                 return Forbid();
         }
-
+        [Authorize()]
         [HttpGet]
         [Route("CountCondition/condition")]
         public IActionResult CountCondition(string condition)
@@ -187,6 +199,7 @@ namespace TemplateWebApiPhucThinh.Controllers
         }
 
 
+        [Authorize()]
         [HttpGet]
         [Route("PagingConditionPrice/condition/pageIndex/pageSize/sortOrder/priceStart/priceEnd")]
         public IActionResult PagingConditionPrice(string condition, int pageIndex, int pageSize, string sortOrder, int priceStart, int priceEnd)
@@ -203,6 +216,7 @@ namespace TemplateWebApiPhucThinh.Controllers
             
         }
 
+        [Authorize()]
         [HttpGet]
         [Route("PagingConditionGetByEmail/pagesize/pageNow/condition")]
         public IActionResult PagingConditionGetByEmail(int pagesize, int pageNow,string condition)
@@ -226,6 +240,8 @@ namespace TemplateWebApiPhucThinh.Controllers
                 }
              return Forbid();
     }
+
+        [Authorize()]
         [HttpGet]
         [Route("CountAllPagingConditionGetByEmail")]
         public IActionResult CountAllPagingConditionGetByEmail()
@@ -246,6 +262,7 @@ namespace TemplateWebApiPhucThinh.Controllers
              return Forbid();
     }
 
+        [Authorize()]
         [HttpGet]
         [Route("CountPagingConditionGetByEmail/condition")]
         public IActionResult CountPagingConditionGetByEmail(string condition)
